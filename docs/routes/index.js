5d02031b16bd76a7934b7c76629b3608f8c7ccf5
Tjas <t.leghissa@gmail.com>
2020-11-22 15:03:38 +0100

    Poprava routes

--- docs/routes/index.js
+++ docs/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const {ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 router.get('/', (req, res) => res.render('hmpg', {
     name: req.user.name
