ff34f6da3265001a96f52e218e946f613fc9d97f
rm3612 <rm3612@student.uni-lj.si>
2020-11-28 16:46:05 +0100

    app server test popravljeno

--- docs/app_server/routes/homepage.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const kontroler = require('../controllers/controller');
-
-router.get('/', kontroler.homepage);
-
-module.exports = router;
