cc6ba3730844000b5938f3d751b1217f06fc4827
tjas98 <t.leghissa@gmail.com>
2020-12-28 18:20:56 +0100

    Pop up tekma zemljevid

--- Angular/app_public/src/app/komponente/tekma/tekma.component.ts
+++ Angular/app_public/src/app/komponente/tekma/tekma.component.ts
@@ -2,6 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router'
 import { WebRequestService } from '../../storitve/web-request.service'
 import { Tekma } from '../../modeli/Tekma'
+import * as L from "leaflet";
+
+const iconRetinaUrl = 'assets/images/marker-icon-2x.png';
+const iconUrl = 'assets/images/marker-icon.png';
+const shadowUrl = 'assets/images/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-tekma',
@@ -10,10 +27,12 @@ import { Tekma } from '../../modeli/Tekma'
 })
 export class TekmaComponent implements OnInit {
 
-  tekma: Tekma;
+  private map;
+
+  tekma: any;
+  loaded = false;
   
 
-  tekma_ = new Tekma("", 0, 0, "", "", "", 0,0,0,"",[""], "", [""]);
 
   constructor(private route: ActivatedRoute, private webReq: WebRequestService) { }
 
@@ -22,11 +41,32 @@ export class TekmaComponent implements OnInit {
       (params: Params) => {
         console.log(params.id)
         this.webReq.getTekma(params.id).subscribe((tekma: Tekma) => {
-          this.tekma_ = tekma[0];
-          
+          this.tekma = tekma[0];
+          this.loaded = true;
+          this.initMap();
         })
       }
     ) 
+
+    
+      
+    
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [this.tekma.lat, this.tekma.lng],
+      zoom: 13
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+    var marker = new L.marker([this.tekma.lat, this.tekma.lng]).addTo(this.map);
   }
 
 }
