93308855bb4a30311864237f344681ae0f1bea24
bizjak3 <bizjak3@gmail.com>
2021-01-03 15:52:18 +0100

    uporabnik popravljen

--- Angular/app.js
+++ Angular/app.js
@@ -45,6 +45,7 @@ app.use((req, res, next) => {
   res.header('X-Frame-Options', 'DENY');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader("Content-Security-Policy", "script-src 'self'");
   next();
 });
 
